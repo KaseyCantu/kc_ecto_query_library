698d88a59315bb496ceba22bf0df8204ff386945
Kasey Cantu <kpc.software.engineer@gmail.com>
2022-05-24 13:29:59 -0500

    Added basic KeyValueStore module as an example of how to use Elixir tasks and recursion

---
+++ lib/key_value_store/key_value_store.ex
@@ -0,0 +1,18 @@
+defmodule KeyValueStore do
+  def start_link do
+    Task.start_link(fn -> loop(%{}) end)
+  end
+
+  defp loop(map) do
+    receive do
+      {:get, key, caller} ->
+        send caller, Map.get(map, key)
+        loop(map)
+      {:put, key, value} ->
+        loop(Map.put(map, key, value))
+      {:list_all, caller} ->
+        send caller, map
+        loop(map)
+    end
+  end
+end
